34a1ea4a2043df4b535b5c647670d781651e2e7a
rkpham <30171670+rkpham@users.noreply.github.com>
2018-11-13 13:12:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var title = document.getElementById("Title")
-var num = Cookies.get("NumClicks") || 1
+var num = 1
+if Cookies.get("NumClicks") !== undefined {
+	num = Cookies.get("NumClicks")
+} else {
+	num = 1
+}
 
 function change() {
 	title.innerHTML = "You have clicked the button "+num+" times"
