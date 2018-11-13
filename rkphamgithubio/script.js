5c538b7afb4a23bc336e2b9681e7a5332f7f1f89
rkpham <30171670+rkpham@users.noreply.github.com>
2018-11-13 12:55:01 -0500

    Delete script.js

--- rkphamgithubio/script.js
+++
@@ -1,9 +0,0 @@
-var title = document.getElementById("Title")
-var num = Cookies.get("NumClicks") || 1
-
-function change() {
-	title.innerHTML = "You have clicked the button "+num+" times"
-	num++
-
-	Cookies.set("NumClicks", num)
-}
