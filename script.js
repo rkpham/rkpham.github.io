b265a2f0b9b4c8af8ad83b3481a8f8e240122392
rkpham <30171670+rkpham@users.noreply.github.com>
2018-11-20 09:06:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-var title = document.getElementById("Title")
-var num = 0
-repeat (
-do {
-	num = Cookies.get("NumClicks")
-}
-while (typeof Cookies.get("NumClicks") != "number")
-	
+let amount = document.getElementById("amount")
 
-title.innerHTML = "You have clicked the button "+num+" times"
+let game = {
+	cost: 1,
+	bought: 0,
+	points: 1,
+	timer: 500
+}
 
 function change() {
-	
-	num++
-	title.innerHTML = "You have clicked the button "+num+" times"
-
-	Cookies.set("NumClicks", num)
+	if (points >= cost) {
+		game.bought += 1
+		game.points -= game.cost
+		game.cost += game.cost
+	}
 }
+
+setInterval(function(){
+	game.points += game.bought
+	amount.innerHTML = game.points
+}, 500)
