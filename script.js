6c50799cde28bca5bb3c26cbc3619f9324473c8c
rkpham <30171670+rkpham@users.noreply.github.com>
2018-11-20 07:57:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var title = document.getElementById("Title")
 var num = 0
-if (typeof Cookies.get("NumClicks") == "number") {
-	num = Cookies.get("NumClicks")
-} else {
-	Cookies.set("NumClicks", 0)
-}
 
 title.innerHTML = "You have clicked the button "+num+" times"
 
 function change() {
+	if (Cookies.get("NumClicks") {
+		if (num != Cookies.get("NumClicks") {
+			num = Cookies.get("NumClicks")
+		}
+	}
+	
 	num++
 	title.innerHTML = "You have clicked the button "+num+" times"
 
